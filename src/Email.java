e512d92a52d8366a7c568ce8939311984fd59c8f
ccaiazzo <ccaiazzo@nyc.yearup.org>
2022-12-15 14:44:12 -0500

    Added comments to code

--- src/Email.java
+++ src/Email.java
@@ -1,34 +1,34 @@
 import java.util.Locale;
 import java.util.*;
 public class Email {
-    private String password;
+    private String password; // Instance variables
     private String company;
     private String firstName;
     private String lastName;
     private String address;
     private int mailBoxCapacity = 150;
 
-    public Email(String firstName, String lastName, String department, String company) {
+    public Email(String firstName, String lastName, String department, String company) { // Default constructor
         this.firstName = firstName;
         this.lastName = lastName;
-        String noSpaceCompany = company.replaceAll("\\s", "").toLowerCase();
+        String noSpaceCompany = company.replaceAll("\\s", "").toLowerCase(); // Takes string and removes spaces, converts to lower case
         this.company = noSpaceCompany;
 
-        char[] nameArray = firstName.toCharArray();
+        char[] nameArray = firstName.toCharArray(); // Takes first name and separates first letter to append to email address
         String firstInitial = String.valueOf(nameArray[0]).toLowerCase();
-        this.address = firstInitial + lastName.toLowerCase() + "@" + this.company + "." + department + ".org";
+        this.address = firstInitial + lastName.toLowerCase() + "@" + this.company + "." + department + ".org"; // Full email address is assembled
 
-        String alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-        StringBuilder stbuilder = new StringBuilder();
+        String alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"; // String that contains possible characters for random password
+        StringBuilder stbuilder = new StringBuilder(); // Creates empty StringBuilder object that will be filled with random characters
         Random random = new Random();
-        while(stbuilder.length() < 30) {
-            int index = (int) (random.nextFloat() * stbuilder.length());
-            stbuilder.append(alphaNumeric.charAt(index));
+        while(stbuilder.length() < 30) { // While loop that determines how many characters are included in random password; in this case 30
+            int index = (int) (random.nextFloat() * stbuilder.length()); // Code that determines random index in alphaNumeric string to pull from
+            stbuilder.append(alphaNumeric.charAt(index)); // Random character is appended to StringBuilder
         }
-        password = stbuilder.toString();
+        password = stbuilder.toString(); // StringBuilder is converted to usable String and becomes password variable
     }
 
-    public void setPassword() {
+    public void setPassword() { // Takes input from user to set new password
         Scanner sc = new Scanner(System.in);
         System.out.println("What would you like the new password to be?");
         String newPassword = sc.nextLine();
@@ -36,24 +36,24 @@ public class Email {
         System.out.println("Password changed successfully.");
     }
 
-    public String getAddress() {
+    public String getAddress() { // Returns address string variable
         System.out.println("The email address is " + address);
         return this.address;
     }
 
-    public int getMailBoxCapacity() {
+    public int getMailBoxCapacity() { // Returns int for mailbox capacity
         System.out.println("The mailbox capacity is " + mailBoxCapacity);
         return this.mailBoxCapacity;
     }
 
-    public String getName() {
+    public String getName() { // Returns user's name as string
         System.out.println("The user's name is " + firstName + " " + lastName);
         String fullName = firstName + " " + lastName;
         return fullName;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // New scanner object
 
         System.out.println("What's your first name?");
         String firstName = scanner.nextLine();
